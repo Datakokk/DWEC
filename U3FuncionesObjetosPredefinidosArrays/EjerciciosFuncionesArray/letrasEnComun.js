ed967af162a6d7980f0fc8d20e3ccc91f5ffc857
Datakokk <fiodorelzarster@gmx.com>
2019-10-20 22:25:42 +0200

    letraEnComun.js terminado

--- U3FuncionesObjetosPredefinidosArrays/EjerciciosFuncionesArray/letrasEnComun.js
+++ U3FuncionesObjetosPredefinidosArrays/EjerciciosFuncionesArray/letrasEnComun.js
@@ -1,10 +1,43 @@
+//Realizar una función que reciba como argumento un array con dos elementos de tipo cadena y devuelva true si en el primer elemento de matriz
+//se encuentran todas las letras del segundo elemento de la matriz. Por ejemplo, ['javascript', 'Javascript'] debe devolver true;
+//['java', 'Javascript'], false; ['Javascript','java'], true.
+//['javascript', 'Javascript'] true;
+//['java', 'Javascript'], false;
+//['Javascript','java'], true.
+
+
 function mismasLetras(array){
-    if(array[0].length > array[1].length) return false;
-
-    //recorrer el pais de las veces.
     
+
+    let array1 = array[0].split('');
+    let array2 = array[1].split('');
+    let resultado = [];
+    let noquiero;
+    if(array1.length > array2.length) return document.write(`No se puede comparar ${array2} con ${array1} porque tiene menos caracteres`);
+
+    for (let i = 0; i < array1.length; i++) {
+       
+        for (let j = 0; j < array2.length ; j++) {
+           
+ 
+                console.log('array1 ' + i +' de '+ ' array2 '+ j+ ' estoy en el segundo array');
+             
+            
+            if(array1[i] == array2[j]){
+                
+                resultado += array1.splice(i,1);
+                noquiero += array2.splice(j,1);
+                console.log('guardo ' + resultado)
+                console.log(array1[i] + " 2ª "+ array2[j]); 
+                j=0;
+            }else{
+                console.log(' no esta ' + array1[i] +' no la quiero ' + " 2ª "+ array2[j]); 
+            }
+             
+        }
+    }
+
+   return document.write(`Estas son las letras que he encontrado: ${resultado} <br>`);
+
+
 }
-
-
-
-
