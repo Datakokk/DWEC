09cd09dfc8cee8493b8f44a192e4734264d4955c
Datakokk <fiodorelzarster@gmx.com>
2019-10-31 20:11:00 +0100

    Ejercicio contarFrutas con funciones de array

---
+++ U4UtilizacionObjetosJS/contarFrutas/frutas.js
@@ -0,0 +1,6 @@
+const frutas = [
+  'limón', 'platano', 'cereza', 'naranja', 'manzana', 'mandarina', 
+  'naranja', 'cereza', 'naranja', 'manzana', 'platano', 'cereza', 'naranja', 'uvas',
+  'uvas', 'manzana', 'platano', 'cereza', 'limón', 'cereza', 'naranja', 'pera',
+  'platano', 'limón', 'pera', 'cereza', 'naranja', 'manzana', 'platano'];
+
