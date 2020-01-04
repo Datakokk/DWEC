1107de7da2afb0dac785ce62dd7658ec6568f3b3
Datakokk <fiodorelzarster@gmx.com>
2020-01-04 17:06:42 +0100

    Ejercicio completo de Intl

---
+++ U6BOM/teoriaProfesor/Intl/datos.js
@@ -0,0 +1,13 @@
+//10/12/2019
+const datos = [
+  {"fecha":"4/1/2019","visitas":938213,"publicidad":3890.21},
+  {"fecha":"4/2/2019","visitas":238213,"publicidad":9890.10},
+  {"fecha":"4/3/2019","visitas":456901,"publicidad":12710},
+  {"fecha":"4/4/2019","visitas":890212,"publicidad":410},
+  {"fecha":"4/5/2019","visitas":1238972,"publicidad":6712.50},
+  {"fecha":"4/6/2019","visitas":392382,"publicidad":99.99},
+  {"fecha":"4/7/2019","visitas":56799,"publicidad":43810},
+  {"fecha":"4/8/2019","visitas":484331,"publicidad":9381},
+  {"fecha":"4/9/2019","visitas":841424,"publicidad":4382.22},
+  {"fecha":"4/10/2019","visitas":1126569,"publicidad":5201}
+];
